70489f083eb9342db4bce7b76d2ba096ac27be2c
Ralf Holly <ralf.holly@approxion.com>
2019-03-23 14:51:13 +0100

    Redundant initialization of i

--- prime_sum.c
+++ prime_sum.c
@@ -4,7 +4,7 @@
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
 int main(void) {
-    unsigned int sum = 0, i = 0;
+    unsigned int sum = 0, i;
     for (i = 0; i < ARRAY_SIZE(PRIME_TABLE); ++i) {
         sum += PRIME_TABLE[i];
     }
